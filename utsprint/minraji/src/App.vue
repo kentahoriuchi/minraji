c6b255a5ceebf94973539ef30c5d2f1e5093bb91
keisuke-Miki <keisuki112@yahoo.co.jp>
2020-05-09 13:16:18 +0900

    youtube動画再生機能の追加

--- utsprint/minraji/src/App.vue
+++ utsprint/minraji/src/App.vue
@@ -4,15 +4,35 @@
       <!-- <myheader></myheader> -->
       <router-view/>
     </p>
+    <youtube :video-id="videoId" ref="youtube" @playing="playing"></youtube> 
+    <button @click="playVideo">play</button>
   </div>
 </template>
 
+<script>
+import Vue from 'vue'
+import VueYoutube from 'vue-youtube'
 
-// <script>
-// import myheader from './components/myheader'
-// export default{
-//   components: {
-//     myheader
-//     }
-// }
-// </script>
+Vue.use(VueYoutube)
+
+export default {
+  data() {
+    return {
+      videoId: '4VEE01EFHFs'
+    }
+  },
+  methods: {
+    playVideo() {
+      this.player.playVideo()
+    },
+    playing() {
+      console.log('we are watching!!!')
+    }
+  },
+  computed: {
+    player() {
+      return this.$refs.youtube.player
+    }
+  }
+}
+</script>
