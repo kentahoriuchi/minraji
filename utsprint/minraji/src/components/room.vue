c6138347221ec966d145d78a06a2289185aaf831
naoaki <naoaki.galaxy@gmail.com>
2020-05-17 18:05:05 +0900

    home change

--- utsprint/minraji/src/components/room.vue
+++ utsprint/minraji/src/components/room.vue
@@ -6,9 +6,7 @@
   <a>みんラジ!</a>
 </h1>
  <ul class="nav-list">
-      <li class="nav-list-item"><a href="Home.vue#home_link2about">About</a></li>
-      <li class="nav-list-item"><a href="Home.vue#home_link2service">Service</a></li>
-      <li class="nav-list-item"><a href="Home.vue#home_link2how2use">How to use</a></li>
+      <li class="nav-list-item"><router-link to="/signout" id="back-home-button"> ホーム</router-link></li>
       <li class="nav-list-item"> <amplify-sign-out></amplify-sign-out></li>
   </ul>	
   <!--<router-link
@@ -23,7 +21,6 @@
     <section>
       <h2>ルーム広場</h2>
       <p>ここはルームに入る待機場所です。「ルームを作成する」ボタンを押したらルームを作成できます。</p>
-      <!--<router-link to="/signout" id="back-home-button"> ホームに戻る</router-link>-->
     </section>
    
     
--- utsprint/minraji/src/components/room_create.vue
+++ utsprint/minraji/src/components/room_create.vue
@@ -5,17 +5,15 @@
 <h1 class="headline">
       <a>みんラジ!</a>
 </h1>
-<ul class="nav-list">
-      <li class="nav-list-item"><a href="#link2about">About</a></li>
-      <li class="nav-list-item"><a href="#link2service">Service</a></li>
-      <li class="nav-list-item"><a href="#link2how2use">How to use</a></li>
+  <ul class="nav-list">
+      <li class="nav-list-item"><router-link to="/signout" id="back-home-button"> ホーム</router-link></li>
       <li class="nav-list-item"> <amplify-sign-out></amplify-sign-out></li>
   </ul>	
 </header>
 <main>
   <section>
     <h2>ルーム作成</h2>
-    <p>ここはルーム作成ページです。youtubeのURLをしたのボックスにいれて「ルームを作る」ボタンを押してください。
+    <p>ここはルーム作成ページです。youtubeのURLを下のボックスにいれて「ルームを作る」ボタンを押してください。
     ルームを作成したら「ルーム広場に戻る」ボタンを押して作成したルームに入ってください</p>
   </section>
 
